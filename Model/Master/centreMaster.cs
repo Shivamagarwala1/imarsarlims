61e030cb3394208768fbdb2104388284ae9341c9
PC <PC@SHUBHAM-ACER>
2024-12-03 11:45:06 +0530

    centremaster

--- Model/Master/centreMaster.cs
+++ Model/Master/centreMaster.cs
@@ -35,11 +35,11 @@ namespace iMARSARLIMS.Model.Master
         public int allowDueReport { get; set; }
         public int reportLock { get; set; }
         public int bookingLock { get; set; }
-        public DateTime unlockTime { get; set; }
+        public DateTime? unlockTime { get; set; }
         public int smsAllow { get; set; }
         public int emailAllow { get; set; }
         [Required, MaxLength(10)]
-        public string? paymentMode { get; set; }
+        public string paymentMode { get; set; }
         public int paymentModeId { get; set; }
         public string? reportHeader { get; set; }
         public string? reciptHeader { get; set; }
@@ -48,19 +48,19 @@ namespace iMARSARLIMS.Model.Master
         public int showBackcover { get; set; }
         [MaxLength(100)]
         public string? reportBackImage { get; set; }
-        public int reporrtHeaderHeightY { get; set; }
-        public int patientYHeader { get; set; }
-        public int barcodeXPosition { get; set; }
-        public int barcodeYPosition { get; set; }
-        public int QRCodeXPosition { get; set; }
-        public int QRCodeYPosition { get; set; }
-        public int isQRheader { get; set; }
-        public int isBarcodeHeader { get; set; }
-        public int footerHeight { get; set; }
-        public int NABLxPosition { get; set; }
-        public int NABLyPosition { get; set; }
-        public int docSignYPosition { get; set; }
-        public int receiptHeaderY { get; set; }
+        public int? reporrtHeaderHeightY { get; set; }
+        public int? patientYHeader { get; set; }
+        public int? barcodeXPosition { get; set; }
+        public int? barcodeYPosition { get; set; }
+        public int? QRCodeXPosition { get; set; }
+        public int? QRCodeYPosition { get; set; }
+        public int? isQRheader { get; set; }
+        public int? isBarcodeHeader { get; set; }
+        public int? footerHeight { get; set; }
+        public int? NABLxPosition { get; set; }
+        public int? NABLyPosition { get; set; }
+        public int? docSignYPosition { get; set; }
+        public int? receiptHeaderY { get; set; }
         [MaxLength(20)]
         public string? PAN { get; set; }
         [MaxLength(16)]
@@ -71,13 +71,13 @@ namespace iMARSARLIMS.Model.Master
         public string? IFSCCode { get; set; }
         public int bankID { get; set; }
         public int salesExecutiveID { get; set; }
-        public int isDefault { get; set; }
-        public int isLab { get; set; }
+        public int? isDefault { get; set; }
+        public int? isLab { get; set; }
         public int minBookingAmt { get; set; }
         public string? lockedBy { get; set; }
-        public DateTime LockDate { get; set; }
+        public DateTime? LockDate { get; set; }
         public string? unlockBy { get; set; }
-        public DateTime unlockDate { get; set; }
+        public DateTime? unlockDate { get; set; }
         public int state { get; set; }
         [MaxLength(20)]
         public string? chequeNo { get; set; }
